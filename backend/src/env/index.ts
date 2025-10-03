f03d6929f57a110b36cfdc1a906042f09024e744
Yaba Ernesto <yabaernesto@gmail.com>
2025-10-04 00:30:14 +0100

    chore: configuração global

---
+++ backend/src/env/index.ts
@@ -0,0 +1,23 @@
+import "dotenv/config"
+import z from "zod"
+
+const port = 3333
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
+  PORT: z.coerce.number().default(port),
+  DATABASE_URL: z.string().url().startsWith("postgresql://"),
+  JWT_SECRET: z.coerce.string(),
+  NASA_API_KEY: z.coerce.string(),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  // biome-ignore lint/suspicious/noConsole: show exception
+  console.log("❌ Invalid environment variables", _env.error.format())
+
+  throw new Error("Invalid environment variables.")
+}
+
+export const env = _env.data
