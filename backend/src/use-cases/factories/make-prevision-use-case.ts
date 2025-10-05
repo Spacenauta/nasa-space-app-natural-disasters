2b0677743837d135d2f2eb36c6da912be0c2a349
Yaba Ernesto <yabaernesto@gmail.com>
2025-10-05 03:57:10 +0100

    chore: comentários removidos

--- backend/src/use-cases/factories/make-prevision-use-case.ts
+++ backend/src/use-cases/factories/make-prevision-use-case.ts
@@ -3,12 +3,9 @@ import { makeGeminiAIUseCase } from "./make-ai-use-case"
 import { makeNasaDataUseCase } from "./make-data-nasa-use-case"
 
 export function makePrevisionUseCase() {
-  // Cria a dependência de Dados (que já carrega Cache e Chave NASA)
   const nasaDataUseCase = makeNasaDataUseCase()
 
-  // Cria a dependência de AI (que já carrega a Chave GEMINI)
   const geminiAiUseCase = makeGeminiAIUseCase()
 
-  // Cria o Orquestrador, injetando as duas dependências
   return new PrevisionUseCase(nasaDataUseCase, geminiAiUseCase)
 }
