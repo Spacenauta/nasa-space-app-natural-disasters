64743d575c5d04500dc2684056d251b49aa223d8
Yaba Ernesto <yabaernesto@gmail.com>
2025-10-05 03:54:20 +0100

    chore: comentários removidos

--- backend/src/use-cases/prevision.ts
+++ backend/src/use-cases/prevision.ts
@@ -6,7 +6,6 @@ type PrevisionUseCaseRequest = {
   longitude: number
 }
 
-// O PrevisionUseCase é o único ponto de entrada para a lógica de negócio
 export class PrevisionUseCase {
   constructor(
     private nasaDataUseCase: NasaDataUseCase,
@@ -17,15 +16,11 @@ export class PrevisionUseCase {
     latitude,
     longitude,
   }: PrevisionUseCaseRequest): Promise<GeminiPrevisionResult> {
-    // OBTENÇÃO DE DADOS (COM CACHE)
-    // O NasaDataUseCase irá buscar os eventos e cuidar do cache automaticamente.
     const nasaEvents = await this.nasaDataUseCase.execute({
       latitude,
       longitude,
     })
 
-    // ANÁLISE E GERAÇÃO DE RISCO (COM GEMINI AI)
-    // O GeminiAIUseCase irá interpretar os dados e gerar o resultado em JSON.
     const prevision = await this.geminiAiUseCase.execute({
       userLatitude: latitude,
       userLongitude: longitude,
