ed571199badbfad746d4318698ba207b66e9e9dc
Yaba Ernesto <yabaernesto@gmail.com>
2025-10-05 03:47:02 +0100

    feat: rota de previsão

--- backend/src/http/controllers/disaster/routes.ts
+++ backend/src/http/controllers/disaster/routes.ts
@@ -1,6 +1,9 @@
 import type { FastifyInstance } from "fastify"
+import { verifyJwt } from "@/http/middlewares/verify-jwt"
+
 import { nasaCategories } from "./nasa-categories"
 import { nasaEvents } from "./nasa-events"
+import { prevision } from "./prevision"
 
 export function disasterRoutes(app: FastifyInstance) {
   // Rota para TESTAR EVENTOS DE DESASTRE (Cache + API Key)
@@ -8,4 +11,11 @@ export function disasterRoutes(app: FastifyInstance) {
   app.get("/nasa/events/:latitude/:longitude", nasaEvents)
   // Rota para TESTAR CATEGORIAS
   app.get("/nasa/categories", nasaCategories)
+
+  // ROTAS DE VALOR (PREVISÃO) - Precisa estar autenticado
+  app.get(
+    "/prevision/:latitude/:longitude",
+    { onRequest: [verifyJwt] },
+    prevision
+  )
 }
